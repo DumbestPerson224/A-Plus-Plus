11b4c90f3ce71d2da56aabb397435a1e25654329
DumbestPerson224 <eTmullall@outlook.com>
2024-12-17 14:01:37 -0500

    Added home page (no CSS)

--- Parser.C
+++ Parser.C
@@ -4,16 +4,16 @@
 #include <ctype.h>
 
 struct instructions {
-   const char* opcodes [100];
-   const char* operands [100];
+   const char* opcodes = new char [1000];
+   const char* operand = new char [1000];
 };
-struct instructions instructions;
+struct instructions* instructions;
 int opcode_index = 0, operand_index = 0;
 
-void get_opcode(const char* opcode){
+void get_opcode(const char *opcode){
    if(opcode_index < 100){
-     if(strcmp(opcode, "kadd") == 0 || strcmp(opcode, "ksub") == 0 || strcmp(opcode, "kmul") == 0 || strcmp(opcode, "kdiv") == 0 || strcmp(opcode, "kjmp") == 0){
-      instructions.opcodes[opcode_index++] = opcode;      
+     if(strcmp(*opcode, "kadd") == 0 || strcmp(*opcode, "ksub") == 0 || strcmp(*opcode, "kmul") == 0 || strcmp(*opcode, "kdiv") == 0 || strcmp(*opcode, "kjmp") == 0){
+      instructions.opcodes[opcode_index++] = *opcode;      
      }else{
           printf("Invalid opcode!\n");
      }
@@ -22,8 +22,9 @@ void get_opcode(const char* opcode){
 
 void get_operands(const char* operand){
      if(opcode_index < 100){
-          if(strcmp(operand, "ah") == 0 || strcmp(operand, "al") == 0 || strcmp(operand, "dh") == 0){
-               instructions.operands[operand_index++] = operand;
+          if(strcmp(*operand, "ah") == 0 || strcmp(*operand, "al") == 0 || strcmp(*operand, "dh") == 0 || strcmp(*operand, "ch") == 0 || strcmp(*operand, "cl") == 0){
+               instructions->operands[operand_index++] = *operand;
           }
      }
 }
+x
