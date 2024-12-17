3de6405ca496d22ed7c988b024b3a85342492755
DumbestPerson224 <eTmullall@outlook.com>
2024-12-17 14:07:53 -0500

    Why?

---
+++ src/language.js
@@ -0,0 +1,13 @@
+function changeLanguage(){
+    var language = document.getElementById("language").innerText; 
+    switch(language){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            console.log(`${language.toString()} is not a supported language`);
+    }
+}
