7f6cdd707f3c5b3eedbd9590fac71c2aef2e35b7
DumbestPerson224 <eTmullall@outlook.com>
2024-12-19 17:59:48 -0500

    Create code_gen.C

---
+++ code_gen.C
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+enum opcodes{
+    ADD_OPERATION,
+    SUBTRACT_OPERATION,
+    MOVE_OPERATION,
+    DIVIDE_OPERATION,
+    MULTIPLY_OPERATION,
+};
+
+int saved_opcode, opcode_index = 0;
+const char* opcodes[10] = {};
+
+void convert_opcode(){
+    switch(saved_opcode){
+        case ADD_OPERATION:
+           opcodes[opcode_index] = "add";
+           printf("+: %s\n", opcodes[opcode_index]);
+           opcode_index += 1;
+           break;
+        
+        case SUBTRACT_OPERATION:
+           opcodes[opcode_index] = "sub";
+           printf("-: %s\n", opcodes[opcode_index]);
+           opcode_index += 1;
+           break;
+           
+        case MOVE_OPERATION:
+           opcodes[opcode_index] = "mov";
+           printf("MOVE: %s\n", opcodes[opcode_index]);
+           opcode_index += 1;
+           break;
+           
+        case DIVIDE_OPERATION:
+           opcodes[opcode_index] = "div";
+           printf("/: %s\n", opcodes[opcode_index]);
+           opcode_index += 1;
+           break;
+           
+        case MULTIPLY_OPERATION:
+           opcodes[opcode_index] = "mul";
+           printf("*: %s\n", opcodes[opcode_index]);
+           opcode_index += 1;
+           break;
+    }
+}
+
+void get_opcode(const char* opcode){
+    if(strcmp(opcode, "+") == 0){
+        saved_opcode = ADD_OPERATION;
+    }else if(strcmp(opcode, "-") == 0){
+        saved_opcode = SUBTRACT_OPERATION;
+    }else if(strcmp(opcode, "move") == 0){
+        saved_opcode = MOVE_OPERATION;
+    }else if(strcmp(opcode, "/") == 0){
+        saved_opcode = DIVIDE_OPERATION;
+    }else if(strcmp(opcode, "*") == 0){
+        saved_opcode = MULTIPLY_OPERATION;
+    }
+    convert_opcode();
+}
